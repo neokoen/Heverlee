c4db66ce788c422089bb20319960d1a55adc741e
koen <neokoen@hotmail.com>
2015-01-20 08:34:17 +0100

    eerste versie

---
+++ Oefeningen/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefeningen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("eerste oefening");
+        }
+    }
+}
